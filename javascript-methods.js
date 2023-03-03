185a468e003e3fc422a06423a70f090729f86989
Usman Mahmood <usmanmahmoodpersonal@gmail.com>
2023-03-03 16:02:31 -0500

    Added Functions

--- javascript-methods.js
+++ javascript-methods.js
@@ -9,51 +9,112 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 ----------------------------------------------------------*/
 
 // MAP //
-Array.prototype.myMap = function(callbackFn) {
-  // Place your code here.
+Array.prototype.myMap = function (callbackFn) {
+  let result = [];
+  this.forEach((value, index, array) => {
+    result.push(callbackFn(value));
+  });
+  return result;
 };
 
 // FILTER //
-Array.prototype.myFilter = function(callbackFn) {
-  // Place your code here.
+Array.prototype.myFilter = function (callbackFn) {
+  let result = [];
+  this.forEach((value, index, array) => {
+    if (callbackFn(value)) {
+      result.push(value);
+    }
+  });
+  return result;
 };
 
 // SOME //
-Array.prototype.mySome = function(callbackFn) {
-  // Place your code here.
+Array.prototype.mySome = function (callbackFn) {
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFn(this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // EVERY //
-Array.prototype.myEvery = function(callbackFn) {
-  // Place your code here.
+Array.prototype.myEvery = function (callbackFn) {
+  this.forEach((value) => {
+    if (callbackFn(value) == false) {
+      return false;
+    }
+  });
+  return true;
 };
 
 // REDUCE //
-Array.prototype.myReduce = function(callbackFn) {
-  // Place your code here.
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  let accumulator;
+  if (initialValue === undefined) {
+    accumulator = undefined;
+  } else {
+    accumulator = initialValue;
+  }
+  for (let i = 0; i < this.length; i++) {
+    if (accumulator !== undefined) {
+      accumulator = callbackFn.call(undefined, accumulator, this[i], i, this);
+    } else {
+      accumulator = this[i];
+    }
+  }
+  if (accumulator === undefined) {
+    throw new TypeError(
+      "Calling myReduce on an empty array with no initial value will return an error"
+    );
+  }
+  return accumulator;
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function(searchElement) {
-  // Place your code here.
+Array.prototype.myIncludes = function (searchElement) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == searchElement) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function(searchElement) {
-  // Place your code here.
+Array.prototype.myIndexOf = function (searchElement) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == searchElement) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function(searchElement) {
-  // Place your code here.
+Array.prototype.myLastIndexOf = function (searchElement) {
+  for (let i = this.length - 1; i > -1; i--) {
+    if (this[i] == searchElement) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 // KEYS //
-Object.myKeys = function(object) {
-  // Place your code here.
+Object.myKeys = function (object) {
+  let theKeys = [];
+  for (let key in object) {
+    theKeys.push(key);
+  }
+  return theKeys;
 };
 
 // VALUES //
-Object.myValues = function(object) {
-  // Place your code here.
+Object.myValues = function (object) {
+  let theValues = [];
+  for (let key in object) {
+    theValues.push(object[key]);
+  }
+  return theValues;
 };
